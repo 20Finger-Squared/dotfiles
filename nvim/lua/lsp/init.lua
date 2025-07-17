68679c347f79b473f0cf2634bf6c29cd68b0f58d
eclipxia <xaylei@outlook.com>
2025-07-17 16:23:01 -0400

    deleted old config

--- nvim/lua/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-local lspconfig = require("lspconfig")
-local servers = {
-  "clangd", "pyright", "lua_ls", "rust_analyzer", "gopls",
-  "jsonls", "bashls", "marksman", "html", "cssls", "zls"
-}
-
-for _, server in ipairs(servers) do
-  lspconfig[server].setup({})
-end
