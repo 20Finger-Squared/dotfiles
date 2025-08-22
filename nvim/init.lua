472cd44123c055c21e8b745b4297b273d3a86241
20Finger-Squared <orhylie@gmail.com>
2025-08-22 06:09:38 +0100

    Moved neovide config and imported lazy config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,28 @@
-vim.opt.showmode = false
-vim.o.guifont = "JetBrainsMono Nerd Font:h14"
-vim.g.neovide_hide_titlebar = true 
-vim.g.neovide_position_animation_length = 0
-vim.g.neovide_cursor_animation_length = 0.00
-vim.g.neovide_cursor_trail_size = 0
-vim.g.neovide_cursor_animate_in_insert_mode = false
-vim.g.neovide_cursor_animate_command_line = false
-vim.g.neovide_scroll_animation_far_lines = 0
-vim.g.neovide_scroll_animation_length = 0.00
-vim.g.neovide_scale_factor = 1.3
-vim.opt.termguicolors = true
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Setup lazy.nvim
+require("lazy").setup("plugins")
 require("settings")
-require("config.lazy")        -- sets up lazy.nvim with only valid plugin specs
 require("keymaps")
 require("autocmds")
