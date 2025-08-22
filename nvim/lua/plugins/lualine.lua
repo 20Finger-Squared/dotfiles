874659bb8203d2b29436c32ffc783a469cfaa06d
20Finger-Squared <orhylie@gmail.com>
2025-08-22 06:06:52 +0100

    Formatting

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,37 +1,30 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      local function terminal_status()
-        if vim.bo.buftype == "terminal" then
-          return "TERMINAL"
-        end
-        return ""
-      end
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            local function terminal_status()
+                if vim.bo.buftype == "terminal" then
+                    return "TERMINAL"
+                end
+                return ""
+            end
 
-      require("lualine").setup({
-        options = {
-          -- theme = "dracula",
-          icons_enabled = true,
-          -- component_separators = { left = "", right = "" },
-          -- section_separators = { left = "", right = "" },
-          -- globalstatus = true,
-          -- component_separators = { left = "|", right = "|" },
-          -- section_separators = { left = "|", right = "|" },
-          component_separators = { left = " ", right = " " },
-          section_separators = { left = " ", right = " " },
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { {"filename", path = 3}, terminal_status },
-          lualine_x = { "encoding", "fileformat", "filetype", "lsp_status" },
-          lualine_y = { "progress" },
-					lualine_z = { "location" },
-        },
-      })
-    end,
-  },
+            require("lualine").setup({
+                options = {
+                    icons_enabled = true,
+                    component_separators = { left = " ", right = " " },
+                    section_separators = { left = " ", right = " " },
+                },
+                sections = {
+                    lualine_a = { "mode" },
+                    lualine_b = { "branch", "diff", "diagnostics" },
+                    lualine_c = { { "filename", path = 3 }, terminal_status },
+                    lualine_x = { "encoding", "fileformat", "filetype", "lsp_status" },
+                    lualine_y = { "progress" },
+                    lualine_z = { "location" },
+                },
+            })
+        end,
+    },
 }
-
